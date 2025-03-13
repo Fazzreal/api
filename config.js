faece8c10da214eb69a52fb4e25396f830ef25c5
Fazzreal <154850252+Fazzreal@users.noreply.github.com>
2025-03-13 07:30:14 +0700

    Create config.js

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    mongoURI: "mongodb+srv://fazz:fazz@tokendb.bekv8.mongodb.net/?retryWrites=true&w=majority&appName=tokenDB", // Ganti dengan MongoDB URI milikmu
+    port: 3000, // Port API
+    secretKey: "your-secure-key" // Kunci rahasia untuk validasi API
+};
